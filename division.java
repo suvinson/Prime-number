436c895843faf589f9634f8b67995251c01cfe11
suvinson <suvinson1209@gmail.com>
2026-01-20 08:23:14 +0530

    Add division program

---
+++ division.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class multi
+{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int a=scan.nextInt();
+        int b=scan.nextInt();
+        int c=scan.nextInt();
+        int d,f,g;
+        d=a+b+c;
+        f=a*b*c;
+        g=f/d;
+        System.out.print(g);
+    }
+}
