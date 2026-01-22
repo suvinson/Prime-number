0a04ddb1f965582c2076c0f827ebf0db404bf422
suvinson <suvinson1209@gmail.com>
2026-01-22 09:15:26 +0530

    code added

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Input {
+
+
+    public static void main(String args[])
+    {
+       System.out.print("Enter the value: ");
+        Scanner name= new Scanner(System.in);
+        int x=name.nextInt();
+         System.out.print("Enter the second value: ");
+        int y=name.nextInt();
+        System.out.print(x+y);
+        
+    }
+    
+}
